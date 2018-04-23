4b31ee2eec484e3d40d3a5b5da37dd4bbcb8d85f
Ceri Storey <ceri.storey@oliverwyman.com>
2018-04-23 17:07:08 +0100

    Highly engaging coffee ordering sim.

--- src/Haskbucks.hs
+++ src/Haskbucks.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Haskbucks where
 
 import Data.IORef
+import Control.Monad.State.Class
+import Control.Monad.Writer.Class
+import Control.Monad.Reader.Class
+
+import GHC.Stack
+
+import Data.Foldable (foldl')
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -9,10 +17,27 @@ newtype OrderId = OrderId (Int)
     deriving (Show, Ord, Eq)
 
 data Cup = ACoffee
+  deriving (Show, Eq)
+
+data CoffeeError =
+    NotReady
+  | AlreadyTaken
+  deriving (Show, Eq)
 
 data CustomerOrder f = CustomerOrder {
-    orderCoffee :: f OrderId
-,   takeCoffee :: OrderId -> f Cup
+    coOrder :: f OrderId
+,   coTake :: OrderId -> f (Either CoffeeError Cup)
+}
+
+
+data OrderEvent =
+    OrderedCoffee
+  | OrderPrepared
+  | OrderDelivered
+  deriving (Show, Eq)
+
+data CashierState = CashierState {
+  prevOrderId :: Int
 }
 
 newCashier :: IO (CustomerOrder IO)
@@ -22,4 +47,36 @@ newCashier = do
 
     where
         doOrder ids = OrderId <$> atomicModifyIORef ids (\i -> (succ i, succ i))
-        doTake _ _id = return ACoffee
+        doTake _ _id = return $ Right ACoffee
+
+data OrderState = 
+    OrderStart
+  | OrderAccepted
+  | OrderReady
+  | OrderCompleted
+  deriving (Show)
+
+pureCashier :: (HasCallStack, MonadReader [OrderEvent] m, MonadState CashierState m, MonadWriter [OrderEvent] m) => CustomerOrder m
+pureCashier = CustomerOrder takeOrder takeCoffee
+  where
+    takeOrder = do
+      order <- OrderId <$> gets prevOrderId
+      modify $ \st -> st { prevOrderId = succ $ prevOrderId st }
+      tell [OrderedCoffee]
+      return order
+    takeCoffee _ = do
+      st <- evalHistory <$> ask
+      case st of
+        OrderReady -> do
+          tell [OrderDelivered]
+          return $ Right ACoffee
+        OrderCompleted -> return $ Left AlreadyTaken
+        _ -> return $ Left NotReady
+
+    evalHistory :: [OrderEvent] -> OrderState
+    evalHistory = foldl' applyEvent OrderStart 
+    applyEvent :: OrderState -> OrderEvent -> OrderState
+    applyEvent OrderStart OrderedCoffee = OrderAccepted
+    applyEvent OrderAccepted OrderPrepared = OrderReady
+    applyEvent OrderReady OrderDelivered = OrderCompleted
+    applyEvent st _ev = st -- error $ "applyEvent: " ++ show (st, ev)
---
+++ test/HaskbucksSpec.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module HaskbucksSpec (spec) where
+
+import           Test.Hspec
+import           Haskbucks
+import qualified Control.Monad.RWS.Strict as RWS
+import Control.Arrow ((***))
+
+spec :: Spec
+spec = parallel $ do
+  describe "PureCashier" $ do
+    it "Records that an order has occurred" $ do
+      let (_, logs) = runCashierPure [] coOrder
+      
+      logs `shouldContain` [OrderedCoffee]
+
+    it "Disallows taking by default" $ do
+      let (res, _) = runCashierPure [] $
+                      \cashier -> do
+                              order <- coOrder cashier
+                              coTake cashier order
+      
+      res `shouldBe` Left NotReady
+    it "Returns coffee when prepared" $ do
+      let (order, hist) = runCashierPure [] $ coOrder
+      let (coffee, _) = runCashierPure (hist ++ [OrderPrepared]) $ flip coTake order
+      
+      coffee `shouldBe` Right ACoffee
+
+    it "Cant take coffee twice" $ do
+      let (order, hist) = runCashierPure [] $ coOrder
+      let (_, hist') = runCashierPure (hist ++ [OrderPrepared]) $ flip coTake order
+      let (coffee, _) = runCashierPure hist' $ flip coTake order
+      
+      coffee `shouldBe` Left AlreadyTaken
+
+  where
+  runCashierPure history action = (id *** (history++)) $ RWS.evalRWS (action pureCashier) history $ CashierState 0
