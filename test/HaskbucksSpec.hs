77f0206a4fb0b8bd75c7675bcf089a8c3453a8f1
Ceri Storey <ceri.storey@oliverwyman.com>
2018-04-24 11:29:53 +0100

    Extract out contract.

--- test/HaskbucksSpec.hs
+++ test/HaskbucksSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
 
 module HaskbucksSpec (spec) where
 
@@ -9,25 +10,27 @@ import           Control.Monad.State.Strict (MonadState, State)
 
 spec :: Spec
 spec = parallel $ do
-  describe "PureCashier" $ do
-    let events = stateLogger
+  describe "Pure" $ cashierContract stateLogger runState
+  where
+  cashierContract :: Monad m => EventLog OrderEvent m -> (forall a . m a -> IO (a, [OrderEvent])) -> Spec
+  cashierContract events run = do
     let cashier = pureCashier events
     let barista = pureBarista events
 
     it "Records that an order has occurred" $ do
-      let (_, logs) = run $ do
+      (_, logs) <- run $ do
            coOrder cashier
 
       logs `shouldContain` [OrderedCoffee]
 
     it "Disallows taking by default" $ do
-      let (res, _) = run $ do
+      (res, _) <- run $ do
             order <- coOrder cashier
             coTake cashier order
 
       res `shouldBe` Left NotReady
     it "Returns coffee when prepared" $ do
-      let (coffee, _) = run $ do
+      (coffee, _) <- run $ do
             order <- coOrder cashier
             bPrepareDrink barista order
             coTake cashier order
@@ -35,7 +38,7 @@ spec = parallel $ do
       coffee `shouldBe` Right ACoffee
 
     it "Cant take coffee twice" $ do
-      let (coffee, _) = run $ do
+      (coffee, _) <- run $ do
             order <- coOrder cashier
             bPrepareDrink barista order
             _ <- coTake cashier order
@@ -43,9 +46,8 @@ spec = parallel $ do
 
       coffee `shouldBe` Left AlreadyTaken
 
-  where
-  run :: State [ev] a -> (a, [ev])
-  run = flip State.runState []
+  runState :: State [ev] a -> IO (a, [ev])
+  runState = pure . flip State.runState []
   stateLogger :: MonadState [a] m => EventLog a m
   stateLogger = EventLog getter writer
     where
