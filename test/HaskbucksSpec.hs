a3b20d131268c3063717f7e3090a0fae0dddf9b8
Ceri Storey <ceri.storey@oliverwyman.com>
2018-05-02 12:15:44 +0100

    Start on a pg implementation.

--- test/HaskbucksSpec.hs
+++ test/HaskbucksSpec.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module HaskbucksSpec (spec) where
 
@@ -10,6 +12,20 @@ import qualified Control.Monad.State.Strict as State
 import           Control.Monad.State.Strict (MonadState, State)
 import           Control.Concurrent.STM (STM)
 import qualified Control.Concurrent.STM as STM
+import qualified Database.PostgreSQL.Simple as Pg
+import qualified Database.PostgreSQL.Simple.FromField as Pg
+import qualified Database.PostgreSQL.Simple.FromRow as Pg
+import qualified Database.PostgreSQL.Simple.ToField as Pg
+import qualified Data.Aeson as JSON
+import           Data.Pool (Pool)
+import qualified Data.Pool as Pool
+import qualified System.Posix.Env.ByteString as Env
+import           Data.Typeable (Typeable)
+import Data.Hashable (hash)
+
+import Control.Exception (bracket)
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as Char8
 import qualified Data.Map.Strict as Map
 import Control.Concurrent.Async
 import Control.Monad
@@ -146,13 +162,68 @@ withStmEvents f = do
 
     pure $ EventLog getter writer
 
+
+data PgLogItem ev = PgLogItem {
+  _lId :: Integer,
+  lValue :: ev
+} deriving (Show, Typeable)
+
+instance (Typeable ev, JSON.FromJSON ev) => Pg.FromRow (PgLogItem ev) where
+  fromRow = PgLogItem <$> Pg.field <*> Pg.fieldWith Pg.fromJSONField
+
+withPgEvents :: (Typeable ev, JSON.ToJSON ev, JSON.FromJSON ev) => (EventLog ev IO -> IO ()) -> IO ()
+withPgEvents f = do
+  pgUrl <- Env.getEnv $  Char8.pack "PG_URL"
+  case pgUrl of
+    Nothing -> do pendingWith "No $PG_URL specified"
+    Just url -> do
+      bracket (newPgPool url) (Pool.destroyAllResources) $ \pool -> do
+        dropPgEvents pool
+        evs <- newPgEvents pool
+        f $ evs
+
+newPgPool :: ByteString -> IO (Pool Pg.Connection)
+newPgPool url = do
+  pool <- Pool.createPool (Pg.connectPostgreSQL url) Pg.close 1 60 5
+  pure pool
+
+newPgEvents :: forall ev . (Typeable ev, JSON.ToJSON ev, JSON.FromJSON ev) => Pool Pg.Connection -> IO (EventLog ev IO)
+newPgEvents pool = do
+  Pool.withResource pool $ setupDb
+  pure $ EventLog getter writer
+  where
+  getter = Pool.withResource pool $ \c -> do
+      rows <- Pg.query_ c "select * from coffee_logs"
+      pure $ fmap lValue rows
+
+  writer ev = Pool.withResource pool $ \c -> do
+    _ <- Pg.execute c "insert into coffee_logs (value) values (?);" $ Pg.Only (Pg.toJSONField ev)
+    pure ()
+
+  setupDb c = do
+    Pg.withTransaction c $ do
+      _ <- Pg.query c "select pg_advisory_xact_lock(?);" (Pg.Only tableHash) :: IO [Pg.Only ()]
+      void $ Pg.execute_ c "CREATE TABLE IF NOT EXISTS coffee_logs (id serial primary key, value jsonb);"
+
+  tableHash = hash ("coffee_logs" :: String)
+
+dropPgEvents :: Pool Pg.Connection -> IO ()
+dropPgEvents pool = do
+  Pool.withResource pool $ \c -> do
+    void $ Pg.execute_ c "DROP TABLE IF EXISTS coffee_logs;"
+
+runPg :: IO a -> IO a
+runPg = id
+
 spec :: Spec
-spec = parallel $ do
+spec = do
   describe "EventLog" $ do
     describe "Pure" $ around withStateEvents $ logContract runState
     describe "STM" $ around withStmEvents $ logContract runStm
+    describe "Pg" $ around withPgEvents $ logContract runPg
   describe "Coffee" $ do
     describe "Pure" $ around withStateEvents $ cashierContract runState
     describe "STM" $ around withStmEvents $ cashierContract runStm
+    describe "Pg" $ around withPgEvents $ cashierContract runPg
   describe "Smoke" $ do
     smokeTest
